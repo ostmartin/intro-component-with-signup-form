ecf36872af12fbed93c5c5e374e64fc4d74d24df
Arseniy2403 <arseniyermoshkin2403@gmail.com>
2023-09-26 15:49:28 +0300

    vars, fonts

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 
-import './style/index.scss';
+import './style/style.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
